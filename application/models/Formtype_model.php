fce4ee858f9e8718400fffe20aa56f1daeac8488
v1ncee <duchateau.vince@gmail.com>
2018-04-18 12:50:13 +0200

    Fixed formtype and wishquestion

---
+++ application/models/Formtype_model.php
@@ -0,0 +1,19 @@
+<?php
+class FormType_model extends CI_Model {
+    
+    function __construct() {
+        parent::__construct();
+    }
+
+    function get($id) {
+        $this->db->where('id', $id);
+        $query = $this->db->get('formType');
+        return $query->row();
+    }
+    
+    function getAllTypes(){
+        $query = $this->db->get('formType');
+        return $query->result();
+    }
+}
+?>
