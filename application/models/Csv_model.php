65f88b441ebd9f551e93cccab50b0a2ab072eb29
v1ncee <duchateau.vince@gmail.com>
2018-04-20 14:41:27 +0200

    Gebruikers importeren af

--- application/models/Csv_model.php
+++ application/models/Csv_model.php
@@ -7,10 +7,18 @@ class Csv_model extends CI_Model
 {
     function __construct() {
         parent::__construct();
+    }
 
+    function get_gebruikers() {
+    	$query = $this->db->get('gebruiker');
+    	if ($query->num_rows() > 0) {
+    		return $query->result_array();
+    	} else {
+    		return FALSE;
+    	}
     }
 
     function insert_csv($data) {
-        $this->db->insert('gebruiker', $data);
+    	$this->db->insert('gebruiker', $data);
     }
 }
