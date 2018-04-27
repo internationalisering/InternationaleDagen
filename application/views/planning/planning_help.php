98114f21897d54d0c1aceccef1c81ab7c53017f5
Tom <r0670352@student.thomasmore.be>
2018-04-27 12:45:55 +0200

    Icoontje verplaatst + hulpfunctie video planning

--- application/views/planning/planning_help.php
+++ application/views/planning/planning_help.php
@@ -1,6 +1,6 @@
 <div class="modal-header">
   <button type="button" class="close" data-dismiss="modal">&times;</button>
-  <h4 class="modal-title">Help</h4>
+  <h4 class="modal-title">Help-functie</h4>
 </div>
 <div class="modal-body">
   <p>In dit filmpje leer je hoe de planning werkt, wat de kleurencombinaties betekenen en hoe je je inschrijft voor verschillende lezingen. Voor elke lezing kan je ook je feedback doorgeven. Wees hierin constructief want dit wordt nagekeken.</p>
